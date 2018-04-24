5129c4b24dde386e23f419ec7885ffb131352eee
Jake B <jake@symbiote.com.au>
2018-04-24 16:27:10 +1000

    Fix more

--- tests/bootstrap-phpstan.php
+++ tests/bootstrap-phpstan.php
@@ -4,9 +4,9 @@ $PROJECT_DIR = dirname(__FILE__).'/../..';
 /**
  * This file is required to setup Silverstripe class autoloader
  */
-$CORE_PATH = $PROJECT_DIR.'/framework/core';
-if (!file_exists($CORE_PATH.'/Core.php')) {
-    echo 'Unable to find "framework" folder for Silverstripe 3.X project.';
+$CORE_FILEPATH = $PROJECT_DIR.'/silverstripe/framework/src/includes/autoload.php';
+if (!file_exists($CORE_FILEPATH)) {
+    echo 'Unable to find "vendor/silverstripe/framework" folder for Silverstripe 4.X project.';
     exit;
 }
-require_once $CORE_PATH.'/Core.php';
+require_once $CORE_FILEPATH;
