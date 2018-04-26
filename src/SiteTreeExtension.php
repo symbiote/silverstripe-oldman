1c08a576de541f8fb980c61d80c434c201c03be7
Jake B <jake@symbiote.com.au>
2018-04-26 16:55:47 +1000

    Fix #12

--- src/SiteTreeExtension.php
+++ src/SiteTreeExtension.php
@@ -5,26 +5,41 @@ namespace Symbiote\Cloudflare;
 use SilverStripe\Core\Injector\Injector;
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\Control\Controller;
+use Symbiote\Cloudflare\CloudflareResult;
 
 class SiteTreeExtension extends DataExtension
 {
     public function onAfterPublish()
     {
-        $result = Injector::inst()->get(Cloudflare::CLOUDFLARE_CLASS)->purgePage($this->owner);
-
-        if (Controller::has_curr()) {
-            $urls = $result->getSuccesses();
-            $errors = $result->getErrors();
-            $response = Controller::curr()->getResponse();
-            $response->addHeader('Oldman-Cloudflare-Cleared-Links', implode(",\n", $urls));
-            if ($errors) {
-                $response->addHeader('Oldman-Cloudflare-Errors', implode(",\n", $errors));
-            }
-        }
+        $cloudflareResult = Injector::inst()->get(Cloudflare::CLOUDFLARE_CLASS)->purgePage($this->owner);
+        $this->addInformationToHeader($cloudflareResult);
     }
 
     public function onAfterUnpublish()
     {
         Injector::inst()->get(Cloudflare::CLOUDFLARE_CLASS)->purgePage($this->owner);
+        $this->addInformationToHeader($cloudflareResult);
+    }
+
+    private function addInformationToHeader(CloudflareResult $cloudflareResult) {
+        if (!Controller::has_curr()) {
+            return false;
+        }
+        if (!$cloudflareResult) {
+            return false;
+        }
+        $result = false;
+        $urls = $cloudflareResult->getSuccesses();
+        $errors = $cloudflareResult->getErrors();
+        $response = Controller::curr()->getResponse();
+        if ($urls) {
+            $response->addHeader('Oldman-Cloudflare-Cleared-Links', implode(",\n", $urls));
+            $result = true;
+        }
+        if ($errors) {
+            $response->addHeader('Oldman-Cloudflare-Errors', implode(",\n", $errors));
+            $result = true;
+        }
+        return $result;
     }
 }
