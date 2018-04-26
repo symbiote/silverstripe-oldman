e94a45fee885f47bcea765487a62ff8b48a7fbc9
Jake B <jake@symbiote.com.au>
2018-04-27 09:11:02 +1000

    Fix #20

--- src/SiteTreeExtension.php
+++ src/SiteTreeExtension.php
@@ -6,17 +6,24 @@ use SilverStripe\Core\Injector\Injector;
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\Control\Controller;
 use Symbiote\Cloudflare\CloudflareResult;
+use SilverStripe\CMS\Controllers\CMSPageEditController;
 
 class SiteTreeExtension extends DataExtension
 {
     public function onAfterPublish()
     {
+        if (!Cloudflare::config()->enabled) {
+            return;
+        }
         $cloudflareResult = Injector::inst()->get(Cloudflare::CLOUDFLARE_CLASS)->purgePage($this->owner);
         $this->addInformationToHeader($cloudflareResult);
     }
 
     public function onAfterUnpublish()
     {
+        if (!Cloudflare::config()->enabled) {
+            return;
+        }
         Injector::inst()->get(Cloudflare::CLOUDFLARE_CLASS)->purgePage($this->owner);
         $this->addInformationToHeader($cloudflareResult);
     }
@@ -28,16 +35,25 @@ class SiteTreeExtension extends DataExtension
         if (!$cloudflareResult) {
             return false;
         }
+        $controller = Controller::curr();
+        // NOTE(Jake): 2018-04-27
+        //
+        // Make this only occur in context of the CMSPageEditController as we don't
+        // want to add headers in a CLI task that purges pages for example.
+        //
+        if (!($controller instanceof CMSPageEditController)) {
+            return false;
+        }
         $result = false;
         $urls = $cloudflareResult->getSuccesses();
         $errors = $cloudflareResult->getErrors();
         $response = Controller::curr()->getResponse();
         if ($urls) {
-            $response->addHeader('Oldman-Cloudflare-Cleared-Links', implode(",\n", $urls));
+            $response->addHeader('oldman-cloudflare-cleared-links', implode(",", $urls));
             $result = true;
         }
         if ($errors) {
-            $response->addHeader('Oldman-Cloudflare-Errors', implode(",\n", $errors));
+            $response->addHeader('oldman-cloudflare-errors', implode(",", $errors));
             $result = true;
         }
         return $result;
